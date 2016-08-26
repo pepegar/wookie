8e499f1c2fb2f0e0ddbf6abc2d4e6a33485651af
Pepe García <jl.garhdez@gmail.com>
2016-08-27 00:05:01 +0200

    add new method to httpClient trait

--- wookie-core/src/main/scala/wookie/httpclient.scala
+++ wookie-core/src/main/scala/wookie/httpclient.scala
@@ -1,8 +1,7 @@
 package wookie
 
-import com.amazonaws.{ Request, AmazonWebServiceResponse }
+import com.amazonaws.{ AmazonServiceException, AmazonWebServiceResponse, Request }
 import com.amazonaws.http.HttpResponseHandler
-
 import cats.MonadError
 
 import scala.concurrent.Future
@@ -12,6 +11,8 @@ object httpclient {
   trait HttpClient {
 
     def exec[A, B](request: Request[A])(implicit H: HttpResponseHandler[AmazonWebServiceResponse[B]]): Future[B]
+
+    def errorResponseHandler: HttpResponseHandler[AmazonServiceException]
 
   }
 
